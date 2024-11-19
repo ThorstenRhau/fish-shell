7c30db1ad2d2a424735f2dadb1fbb0d555d519e7
Thorsten Rhau <thorsten@rhau.se>
2024-11-19 20:00:19 +0100

    Merged old and new config

--- config.fish
+++ config.fish
@@ -1,39 +1,60 @@
 if status is-interactive
+    # Set environment variables
+    set -gx PAGER less
+
     # Commands to run in interactive sessions can go here
     set -U fish_greeting # Disable greeting function
 
     # Homebrew setup
     if test -d /opt/homebrew
-      set -gx ARCHFLAGS "-arch arm64"
-      set -gx HOMEBREW_PREFIX "/opt/homebrew"
-      set -gx HOMEBREW_NO_ANALYTICS 1
-      set -gx PATH "/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-      if test -f "$HOME/.brew_api_token"
-        source "$HOME/.brew_api_token"
-      end
+        set -gx ARCHFLAGS "-arch arm64"
+        set -gx HOMEBREW_PREFIX "/opt/homebrew"
+        set -gx HOMEBREW_NO_ANALYTICS 1
+        set -gx PATH "/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+        if test -f "$HOME/.brew_api_token"
+            source "$HOME/.brew_api_token"
+        end
     end
 
     # General variables
-    set -gx MANPAGER "less"
+    set -gx MANPAGER "nvim +Man! -"
     set -gx GREP_OPTIONS "--color=auto"
     set -gx LC_CTYPE "en_US.UTF-8"
     set -gx LANG "en_US.UTF-8"
     set -gx PATH "$HOME/bin:$PATH"
 
-    # SSH aliases
-    alias trhau='mosh --ssh="ssh -C -p 9898" thorre@helio.home -- tmux -2 attach'
-    alias srhau='ssh -C -p 9898 -t thorre@helio.home tmux -2 attach'
-
     # exa setup (replacement for ls)
     if type -q exa
-      alias ls "exa -F --git --group-directories-first --color=auto"
+        alias ls "exa -F --git --group-directories-first --color=auto"
     end
 
     # NeoVIM setup with fallback to vim
     if type -q nvim
-      set -gx EDITOR "nvim"
-      alias nv "nvim"
+        set -gx EDITOR "nvim"
+        set -gx VISUAL $EDITOR
+        alias nv "nvim"
     else if type -q vim
-      set -gx EDITOR "vim"
+        set -gx EDITOR "vim"
+    end
+
+    # Add directories to PATH
+    fish_add_path $HOME//bin
+    fish_add_path /usr/local/bin
+    fish_add_path $HOME/.local/bin
+
+    # Set aliases
+    alias ll='ls -lah'
+    alias g='git'
+    alias dc='docker-compose'
+    alias nv='nvim'
+
+    zoxide init fish | source
+    fzf --fish | source
+    starship init fish | source
+
+    # Configure fish greeting
+    function fish_greeting
+        echo "Welcome to fish, Thorsten! 🐟"
     end
 end
+
