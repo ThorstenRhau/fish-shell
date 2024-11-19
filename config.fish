ddee25306a1b647de8d48576b70057e0130ce558
Thorsten Rhau <thorsten@rhau.se>
2024-11-19 20:52:40 +0100

    Added PATHs and light-dark mode detection

--- config.fish
+++ config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
+    # Add directories to PATH
+    fish_add_path $HOME/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path /usr/local/bin
+    fish_add_path /Applications/WezTerm.app/Contents/MacOS
+
     # Set environment variables
     set -gx PAGER less
 
@@ -10,7 +16,8 @@ if status is-interactive
         set -gx ARCHFLAGS "-arch arm64"
         set -gx HOMEBREW_PREFIX "/opt/homebrew"
         set -gx HOMEBREW_NO_ANALYTICS 1
-        set -gx PATH "/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+        fish_add_path -p /opt/homebrew/sbin
+        fish_add_path -p /opt/homebrew/bin
         if test -f "$HOME/.brew_api_token"
             source "$HOME/.brew_api_token"
         end
@@ -21,11 +28,11 @@ if status is-interactive
     set -gx GREP_OPTIONS "--color=auto"
     set -gx LC_CTYPE "en_US.UTF-8"
     set -gx LANG "en_US.UTF-8"
-    set -gx PATH "$HOME/bin:$PATH"
+    set -gx TERM "wezterm"
 
-    # exa setup (replacement for ls)
-    if type -q exa
-        alias ls "exa -F --git --group-directories-first --color=auto"
+    # eza setup (replacement for ls)
+    if type -q eza
+        alias ls "eza --header --git"
     end
 
     # NeoVIM setup with fallback to vim
@@ -37,22 +44,41 @@ if status is-interactive
         set -gx EDITOR "vim"
     end
 
-    # Add directories to PATH
-    fish_add_path $HOME//bin
-    fish_add_path /usr/local/bin
-    fish_add_path $HOME/.local/bin
 
     # Set aliases
-    alias ll='ls -lah'
-    alias g='git'
-    alias gca='git commit -a'
-    alias gst='git status'
-    alias dc='docker-compose'
     alias nv='nvim'
+    alias gst='git status'
+    alias gl='git pull'
+    alias gp='git push'
+    alias glog='git log --oneline --graph --decorate -n 20'
+    alias gca='git commit -a'
+    alias python='python3'
+    alias pip='pip3'
 
     zoxide init fish | source
+
     fzf --fish | source
+
+    set -gx STARSHIP_SHELL fish
     starship init fish | source
+
+    # Zoxide interactive via ctrl-z
+    bind \cz zi 
+
+    # Read the AppleInterfaceStyle preference for light/dark appearance
+    set -l appearance (defaults read -g AppleInterfaceStyle 2>/dev/null)
+
+    if test "$appearance" = "Dark"
+            # Dark theme stuff
+            set -gx DELTA_FEATURES "dark-mode"
+            set -gx LS_COLORS (vivid generate catppuccin-macchiato)
+        echo dark mode set
+    else
+            # Light theme stuff
+            set -gx DELTA_FEATURES "light-mode"
+            set -gx LS_COLORS (vivid generate catppuccin-latte)
+        echo light mode set
+    end
 
     # Configure fish greeting
     function fish_greeting
