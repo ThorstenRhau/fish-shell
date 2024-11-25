a6a16087d2dae1c4d201a3b852d69904b3b156b5
Thorsten Rhau <thorsten@rhau.se>
2024-11-25 13:22:32 +0100

    New alias for cd and cleaned up comments

--- config.fish
+++ config.fish
@@ -1,15 +1,16 @@
 if status is-interactive
-    # Add directories to PATH
+    #                                    ╭──────╮
+    #                                    │ PATH │
+    #                                    ╰──────╯
     set -x fish_user_paths
     fish_add_path $HOME/bin
     fish_add_path $HOME/.local/bin
     fish_add_path /usr/local/bin
     fish_add_path /Applications/WezTerm.app/Contents/MacOS
 
-    set -gx fish_greeting # Disable greeting function
-    set -gx fish_history_limit 100000
-
-    # Homebrew setup
+    #                                  ╭──────────╮
+    #                                  │ Homebrew │
+    #                                  ╰──────────╯
     if test -d /opt/homebrew
         set -gx ARCHFLAGS "-arch arm64"
         set -gx HOMEBREW_PREFIX "/opt/homebrew"
@@ -21,19 +22,27 @@ if status is-interactive
         end
     end
 
-    # General variables
+    #                                  ╭───────────╮
+    #                                  │ Variables │
+    #                                  ╰───────────╯
+    set -gx fish_greeting # Disable greeting function
+    set -gx fish_history_limit 100000
     set -gx GREP_OPTIONS "--color=auto"
     set -gx LC_CTYPE "en_US.UTF-8"
     set -gx LANG "en_US.UTF-8"
     set -gx TERM "wezterm"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
 
-    # eza setup (replacement for ls)
+    #                                     ╭─────╮
+    #                                     │ eza │
+    #                                     ╰─────╯
     if type -q eza
         alias ls "eza --header --git"
     end
 
-    # NeoVim as editor for everything, if installed
+    #                                   ╭────────╮
+    #                                   │ neovim │
+    #                                   ╰────────╯
     if type nvim > /dev/null
         abbr nv 'nvim'
         set -gx EDITOR (which nvim)
@@ -42,21 +51,25 @@ if status is-interactive
         set -gx MANPAGER "nvim +Man! -"
     end
 
-    # Loading secrets if they exist
+    #                                   ╭─────────╮
+    #                                   │ Secrets │
+    #                                   ╰─────────╯
     set secrets_file "$HOME/.config/fish/secrets.fish"
     if test -r $secrets_file
         source $secrets_file
     end
 
-    # bat pager setup, if installed
+    #                                     ╭─────╮
+    #                                     │ bat │
+    #                                     ╰─────╯
     if type bat > /dev/null
         alias cat=bat
         set -gx PAGER bat
     end
 
-    #       ╭───────────────────────────╮
-    #       │ Aliases and abbreviations │
-    #       ╰───────────────────────────╯
+    #                               ╭────────────────╮
+    #                               │ abbr and alias │
+    #                               ╰────────────────╯
     abbr gst 'git status'
     abbr gl 'git pull'
     abbr gp 'git push'
@@ -72,43 +85,36 @@ if status is-interactive
     abbr trhau 'env TERM="xterm-256color" mosh --ssh="ssh -C -p 9898" thorre@helio.home -- tmux -2 attach'
     abbr srhau 'env TERM="xterm-256color" ssh -C -p 9898 -t thorre@helio.home tmux -2 attach || env TERM="xterm-256color" ssh -C -p 9898 -t thorre@helio.home tmux -2 -u new'
 
-    #         ╭─────────────────────────────╮
-    #         │ Zoxide setup and keybinding │
-    #         ╰─────────────────────────────╯
+    #                                   ╭────────╮
+    #                                   │ zoxide │
+    #                                   ╰────────╯
     zoxide init fish | source
     bind \cz zi 
+    alias cd z
 
-    #         ╭─────────────────────────╮
-    #         │ fzf setup and variables │
-    #         ╰─────────────────────────╯
+    #                                     ╭─────╮
+    #                                     │ fzf │
+    #                                     ╰─────╯
     fzf --fish | source
-    # Default command for fzf when no input is provided
     set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git'
-    # Command specifically for the Ctrl-T key binding
     set -gx FZF_CTRL_T_COMMAND 'fd --type f --hidden --exclude .git'
-    # Options for fzf when used with Ctrl-T
     set -gx FZF_CTRL_T_OPTS '--preview "bat --style=numbers --color=always {}" --preview-window=right:60% --border'
-    # Command for listing directories with Alt-C
     set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --exclude .git'
-    # Options for fzf when used with Alt-C
     set -gx FZF_ALT_C_OPTS '--preview "ls -al {}" --preview-window=down:40%'
-    # Options for reverse search with Ctrl-R
     set -gx FZF_CTRL_R_OPTS '--height 40% --layout=reverse --info=inline --border'
-    # Global default options for fzf
     set -gx FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border'
-    # Enable fzf integration with tmux
     set -gx FZF_TMUX 0
-    # Trigger for fzf-based completions
     set -gx FZF_COMPLETION_TRIGGER '**'
 
-    #         ╭───────────────────────╮
-    #         │ Starship prompt setup │
-    #         ╰───────────────────────╯
+    #                                  ╭──────────╮
+    #                                  │ Starship │
+    #                                  ╰──────────╯
     set -gx STARSHIP_SHELL fish
     starship init fish | source
 
-
-    # Read the AppleInterfaceStyle preference for light/dark appearance
+    #                       ╭────────────────────────────────╮
+    #                       │ macOS light / dark theme setup │
+    #                       ╰────────────────────────────────╯
     set -l appearance (defaults read -g AppleInterfaceStyle 2>/dev/null)
 
     if test "$appearance" = "Dark"
