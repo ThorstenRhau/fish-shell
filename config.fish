dde204d75d75bb2559af0eb117a490b214ac0f99
Thorsten Rhau <thorsten@rhau.se>
2024-12-08 21:17:23 +0100

    Mosh environment MOSH_ENABLE_WIDECHAR

--- config.fish
+++ config.fish
@@ -41,6 +41,7 @@ if status is-interactive
     if test -d /Applications/WezTerm.app
         set -gx TERM "wezterm"
     end
+    set -gx MOSH_ENABLE_WIDECHAR 1
 
     #                                     ╭─────╮
     #                                     │ eza │
